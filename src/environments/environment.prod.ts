0c505b5a24ffca8fa4374bb7b2b5178033b97b4b
murugesantechver <murugeshan.r@techversantinfotech.com>
2024-11-14 17:09:08 +0530

    setup env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDxs7OYlz4-NIFBNc3N8Zeikbux7TdIPfk',
-    authDomain: 'sam-app-82bb5.firebaseapp.com',
-    projectId: 'sam-app-82bb5',
-    storageBucket: 'sam-app-82bb5.firebasestorage.app',
-    messagingSenderId: '917374050422',
-    appId: '1:917374050422:web:72bbde07c514c7fa16bf57',
+    apiKey: 'your-actual-api-key',
+    authDomain: 'your-actual-auth-domain',
+    projectId: 'your-actual-project-id',
+    storageBucket: 'your-actual-storage-bucket',
+    messagingSenderId: 'your-actual-messaging-sender-id',
+    appId: 'your-actual-app-id',
   },
+  encryptionSecret: 'your-actual-secret-key',
 };
