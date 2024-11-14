5984c3b09aa4bf90834a2bacd9d8a5206954e074
murugesantechver <murugeshan.r@techversantinfotech.com>
2024-11-14 17:18:26 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
